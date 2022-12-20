397559328b750a2a98ca41556cabb58a17d81e4c
minhtuan1210 <121111562+minhtuan1210@users.noreply.github.com>
2022-12-20 23:59:21 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const CONFIG = {
-    introTitle: 'Babe à!',
+    introTitle: 'Baby!',
     introDesc: `Em đừng cười lại nụ cười ấy nữa
     Nắng tự ti và hoa sẽ dỗi hờn
     Em đừng nhìn anh bằng đôi mắt ấy nữa
@@ -7,8 +7,8 @@ const CONFIG = {
     btnIntro: 'Love <3',
     title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
     desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, Anh trai à :3',
+    btnYes: 'Yêu anh <33',
+    btnNo: 'Không :3',
     question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
     btnReply: 'Gửi cho anh <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
